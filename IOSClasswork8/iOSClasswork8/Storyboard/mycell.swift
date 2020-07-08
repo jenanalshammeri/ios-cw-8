891c6791837cb5c0f483452a060ea94a048b38c5
jenan alshammeri <jenan.alshammeri1@gmail.com>
2020-07-08 21:03:32 +0300

    doneee

---
+++ IOSClasswork8/iOSClasswork8/Storyboard/mycell.swift
@@ -0,0 +1,17 @@
+//
+//  mycell.swift
+//  iOSClaswork8
+//
+//  Created by Jenan Alshammeri on 7/8/20.
+//  Copyright © 2020 Safeyah Coding. All rights reserved.
+//
+
+import UIKit
+
+class mycell: UITableViewCell {
+
+    @IBOutlet weak var cardpic: UIImageView!
+    
+    @IBOutlet weak var cardLabel: UILabel!
+    
+}
